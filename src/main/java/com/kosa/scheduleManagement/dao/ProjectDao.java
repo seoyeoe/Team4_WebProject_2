acf5f4265eb4a05c8bbf944a6147ce52db5f85b1
Gunhee Lee <rjsgml9799@gmail.com>
2023-06-28 08:57:34 +0900

    Edit:Dao

--- src/main/java/com/kosa/scheduleManagement/dao/ProjectDao.java
+++ src/main/java/com/kosa/scheduleManagement/dao/ProjectDao.java
@@ -1,5 +1,17 @@
 package com.kosa.scheduleManagement.dao;
 
+import java.util.List;
+
+import com.kosa.scheduleManagement.dto.Emp;
+import com.kosa.scheduleManagement.dto.Project;
+
 public interface ProjectDao {
+	//로그인한 사원의 프로젝트 리스트
+	List<Project> getProjectList(int deptno);
 	
+	//프로젝트 생성
+	void insertProject(Project project);
+	
+	//프로젝트 삭제
+	void deleteProject(int project_num);
 }
